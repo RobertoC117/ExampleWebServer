f0aeb532cf116ef1544f183831205b69d7b7d990
Roberto Carlos <rcsh.031100@outlook.com>
2021-01-10 23:17:08 -0600

    Agrege comentarios descriptivos en server.js

--- server.js
+++ server.js
@@ -10,16 +10,21 @@ app.use(express.static(__dirname + '/public'))
 //Partials
 hbs.registerPartials(__dirname + '/views/partials')
 
+//middleware para usar hbs para renderizar las vistas
 app.set('view engine', 'hbs')
 
+//respuesta para una peticion get en la direccion /
 app.get('/', (req, res) => {
+    //renderiza el home(hbs) y pasa como parametros la variable del nombre
     res.render('home',{
                         nombre:"roberto carlos sánchez hernández"
                     }
     );
 })
 
+//respuesta para una peticion get en la direccion /about
 app.get('/about', (req, res) => {
+    //renderiza el about(hbs)
     res.render('about');
 })
 
